a6decc0577300068708fafea4c0e3650b53c4a2f
Sébastien Jacques <sebastien.libre@free.fr>
2015-08-25 17:54:23 +0200

    RenderStyles du sharedlayout + divers

---
+++ Form115/Scripts/Hotel/Details.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $("#champDateDepart").datepicker({
+        format: "dd/mm/yyyy",
+        todayBtn: "linked",
+        language: "fr",
+        autoclose: true,
+        todayHighlight: true,
+        endDate: "-1d"
+    });
+});
